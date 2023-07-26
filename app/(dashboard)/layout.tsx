6225475ed2830965bc7a6bda08c6b8cf370d02c3
chem.chikweze <53623147+chem-chikweze@users.noreply.github.com>
2023-07-25 21:06:22 -0400

    wrapping clerk to the app

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,14 @@
+const DashboardLayout = ({
+    children
+}: {
+    children: React.ReactNode
+}) => {
+    return (
+        <div className="h-full relative" >
+        
+
+        </div>
+    );
+}
+
+export default DashboardLayout
