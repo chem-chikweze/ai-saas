78e680a5961b314f882f21bea538779350149f71
chem.chikweze <53623147+chem-chikweze@users.noreply.github.com>
2023-07-25 21:34:00 -0400

    built layout bar, added log and text

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -5,10 +5,7 @@ import { UserButton } from "@clerk/nextjs";
 const DashboardPage = () => {
   return (
     <>
-      <p className="text-6l text-green-500">
-        Dashboar (Protected) Hello AI Saas
-      </p>
-      <UserButton afterSignOutUrl="/" />
+      <p className="text-6l text-green-500">Dashboar (Protected)</p>
     </>
   );
 };
