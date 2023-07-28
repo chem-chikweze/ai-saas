82e16d79d51a2cd02c9416a4554b89c1df83c070
chem.chikweze <53623147+chem-chikweze@users.noreply.github.com>
2023-07-27 21:32:34 -0400

    Form UI for Conversation

---
+++ components/heading.tsx
@@ -0,0 +1,31 @@
+import { cn } from "@/lib/utils";
+
+interface HeadingPros {
+  title: string;
+  description: string;
+  icon: Icon;
+  iconColor?: string;
+  bgColor?: string;
+}
+
+export const Heading = ({
+  title,
+  description,
+  icon,
+  iconColor,
+  bgColor,
+}: HeadingPros) => {
+  return (
+    <>
+      <div className="px-4 lg:px-8 flex items-center gap-x-3 mb-8">
+        <div className={cn("p-2 w-fit rounded-md", bgColor)}>
+          {/* <Icon className={cn("w-1- h-10", iconColor)} /> */}
+        </div>
+        <div>
+          <h2 className="text-3xl font-bold">{title}</h2>
+          <p className="text-sm text-muted-foreground">{description}</p>
+        </div>
+      </div>
+    </>
+  );
+};
